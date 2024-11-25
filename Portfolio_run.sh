84d47d00a153422d69bbe275f837546ffb41f238
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-11-25 12:46:07 +0530

    Portfolio_run.sh

---
+++ Portfolio_run.sh
@@ -0,0 +1,18 @@
+#usr/bin
+#usr/sbin
+
+# Created By : Abhishek Sharma
+# Massage: This is Portfolio Webpage Executeing using Docker file
+
+
+docker build -t portfolio:v1 .
+docker compose up  portfolio -d
+
+if [ $? == 0 ] 
+  docker images | grep -i portfolio | awk -F" " '{print $1,$2}' OFS=":" 
+  
+else
+  echo "Docker compose file Error Code $?"
+fi
+
+
