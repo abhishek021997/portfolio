c08be4ee3cde23b015799971cca49b540a11bcdf
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-11-26 17:47:30 +0530

    Update Portfolio_run.sh

--- Portfolio_run.sh
+++ Portfolio_run.sh
@@ -1,19 +1,30 @@
 #usr/bin
 #usr/sbin
 
-# Created By : Abhishek Sharma
-# Massage: This is Portfolio Webpage Executeing using Docker file
+# Created By: Abhishek Sharma
+# Massage: This is Portfolio Webpage Executing using NGINX 
+
+con_name="portfolio"
+domain_name="byteportfolio.in"
 
 
 ##################### Docker Compose file run ######################
-docker compose up  portfolio -d
+docker compose up -d
 ####################################################################
 
-#if [ $? == 0 ] 
-#  docker images | grep -i portfolio | awk -F" " '{print $1,$2}' OFS=":" 
-  
-#else
-#  echo "Docker compose file Error Code $?"
-#fi
+ val1=$(docker ps -l | grep -i $con_name | wc -l)
+ docker ps -l | grep -i  $con_name | awk -F" " '{print $1}' > container_id
+ docker exec -it $con_name sed -i "s/localhost/ $domain_name /" /etc/nginx/conf.d/default.conf
+ docker exec -it $con_name certbot --nginx
+ ########################
+  # docker exec -it $con_name certbot --nginx (this certificate valid to 90 days)  {certbot renew} --> this is certificate renew command 
+  # After this command provide all SSL certificate information
+  # If not getting any error so SSL is working properly
+  # NOW you run your the command "curl https://byteportfolio.in" (IF this command show us our HTML page that's mean all configuration is working properly)
+ ########################
+ 
+ 
+ 
+ 
 
 
